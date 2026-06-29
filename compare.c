a09b1a3053d2a6e1b614ec3b41cc6fce5001a4f4
anton <antob4you@gmail.com>
2026-06-29 13:01:03 -0400

    initial commit

---
+++ compare.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "cs50.h"
+
+int main(void){
+
+    int x = get_int("what is x? ");
+    int y = get_int("what is y? ");
+
+    if (x<y) {
+        printf("x is less than y\n");
+    }
+    else if (x>y) {
+        printf("x is greater than y\n");
+    }
+    else{
+        printf("x is equal to y");
+    }
+}
