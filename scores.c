280959d5433eae430ccb88ce5fe11ccc3fe4f6ce
anton <antob4you@gmail.com>
2026-07-15 12:22:12 -0400

    update

--- scores.c
+++ scores.c
@@ -1,3 +1,11 @@
-//
-// Created by anton on 7/15/2026.
-//
+#include "cs50.h"
+#include <stdio.h>
+
+int main(void) {
+    int scores[3];
+    scores[0] = 23;
+    scores[1] = 35;
+    scores[2] = 50;
+
+    printf("Average: %f.00\n", (scores[0] + scores[1] + scores[2])/ 3.0);
+}
