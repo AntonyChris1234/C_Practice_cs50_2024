7f3e7b70cb0ff1f734557a01a6b8e171c0304c78
anton <antob4you@gmail.com>
2026-07-17 16:32:26 -0400

    update

---
+++ phonebook.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "cs50.h"
+#include <string.h>
+
+int main(void) {
+    string names[] = {"Carter", "David", "John" };
+    string numbers[] = {"+1-617-491-1000", "+1-617-491-1000", "+1-949-468-2750"};
+
+    
+}
