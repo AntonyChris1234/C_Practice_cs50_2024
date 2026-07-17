99c3d9cc69d9b4905013ec3f0a3c0eeee573f5c1
anton <antob4you@gmail.com>
2026-07-17 13:19:09 -0400

    update

---
+++ length.c
@@ -0,0 +1,8 @@
+#include "cs50.h"
+#include <stdio.h>
+
+int main(void) {
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+}
--- scores.c
+++ scores.c
@@ -1,11 +1,22 @@
 #include "cs50.h"
 #include <stdio.h>
 
-int main(void) {
-    int scores[3];
-    scores[0] = 23;
-    scores[1] = 35;
-    scores[2] = 50;
+float average(int length, int array[]);
 
-    printf("Average: %f.00\n", (scores[0] + scores[1] + scores[2])/ 3.0);
+int main(void) {
+    const int N = 3;
+    int scores[N];
+    for (int i = 0; i < N; i++) {
+        scores[i] = get_int("Score: ");
+    }
+    printf("Average: %f\n", average(N, scores));
+
+}
+
+float average(int length, int array[]){
+    int sum = 0;
+    for (int i =0; i < length; i++) {
+        sum+= array[i];
+    }
+    return sum / (float) length;
 }
