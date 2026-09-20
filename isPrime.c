5652f070aadf1aa105c559c7f5cdb5fc90a73351
anton <antob4you@gmail.com>
2026-09-19 21:54:46 -0400

    update

--- isPrime.c
+++ isPrime.c
@@ -3,27 +3,27 @@
 int isPrime(int num);
 int main(int argc, char* argv[])
 {
-    isPrime(15);
+    for (int i = 0; i < 100; i++)
+    {
+        isPrime(i);
+    }
 }
 
 int isPrime(int num)
 {
-    if(num%2 == 0)
+    if(num%2 == 0 && num != 1 && num != 2)
     {
-        printf("%d is not a prime number", num);
+        printf("%d is not a prime number\n", num);
         return 0;
     }
-    else
-    {
         for(int i = 3; i <= 9; i+=2)
         {
-            if(num % i == 0)
+            if(num % i == 0 && i != num)
             {
-                printf("%d is not a prime number", num);
+                printf("%d is not a prime number\n", num);
                 return 0;
             }
         }
-        printf("%d is a prime number", num);
+        printf("%d is a prime number\n", num);
         return 0;
-    }
 }
